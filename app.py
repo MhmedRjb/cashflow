f247c0def2c625eae409808e0ac8aee62836f1ad
mohamed <mohamed2elfateh@gmail.com>
2023-05-24 03:27:44 +0300

    jj

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from Dataprocessor import DataProcessor as dprs
 from DataBaseConnection import DatabaseExporter as dbcon
-from flask import Flask, render_template , request
+from main import process_good_transection, processor_Clints
+from flask import Flask, render_template , request ,flash
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from wtforms.validators import input_required ,ValidationError,Optional
@@ -10,37 +11,56 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY']='123456789'
 app.config['UPLOAD_FOLDER'] = r'D:\monymovment\Cashflows\static\files'
-def validate_file_extension(form, field):
-    if field.data:
-        filename = field.data.filename
-        ext = os.path.splitext(filename)[1]
-        if ext not in ['.xls', '.xlsx']:
-            raise ValidationError('ملفات أكسل فقط xls, xlsx')
 
-class UploadForm(FlaskForm):
-    file = FileField('SBJRNLITMRPTTAX.xls', validators=[Optional(), validate_file_extension])
-    file2 = FileField('file2', validators=[Optional(), validate_file_extension])
-    submit = SubmitField('Upload file')
+class FileHandler :
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    form = UploadForm()
-    if request.method == 'POST':
-        file = form.file.data
-        file2 = form.file2.data
+    def __init__(self, upload_folder,ALLOWED_EXTENSIONS):
+        self.upload_folder = upload_folder
+        self.ALLOWED_EXTENSIONS = ALLOWED_EXTENSIONS
+    def is_valid_file(self, file):
         if file:
             filename = file.filename
             ext = os.path.splitext(filename)[1]
-            if ext in ['.xls', '.xlsx']:
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-        if file2:
-            filename = file2.filename
-            ext = os.path.splitext(filename)[1]
-            if ext in ['.xls', '.xlsx']:
-                file2.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file2.filename)))
+            return ext in self.ALLOWED_EXTENSIONS
+        return False
 
-    return render_template("home.html", form=form)
+    def save_file(self, file):
+        if self.is_valid_file(file):
+            file.save(os.path.join(self.upload_folder, secure_filename(file.filename)))
+        else:
+            raise ValidationError('Invalid file type. Only .xls and .xlsx files are allowed.')
+
+file_handler = FileHandler(app.config['UPLOAD_FOLDER'], ALLOWED_EXTENSIONS=['.xls', '.xlsx'])
+class UploadForm(FlaskForm):
+    file = FileField('SBJRNLITMRPTTAX.xls', validators=[Optional(), file_handler.is_valid_file])
+    file2 = FileField('file2', validators=[Optional(), file_handler.is_valid_file])
+    submit = SubmitField('Upload file')
+    @app.route('/process_good_transection')
+    def index():
+        processor_goods_transection = process_good_transection()
+        data = processor_goods_transection.data
+        # do something with the processed data
+        return render_template('index.html', data=data)
+
+    @app.route('/', methods=['GET', 'POST'])
+    @app.route('/home', methods=['GET', 'POST'])
+    def home():
+        form = UploadForm()
+        if request.method == 'POST':
+            try:
+                file_handler.save_file(form.file.data)
+                file_handler.save_file(form.file2.data)
+            except ValidationError as e:
+                flash(str(e))
+
+        return render_template("home.html", form=form)
+
+@app.route('/process_good_transection')
+def process_good_transection_route():
+    processor_goods_transection = process_good_transection()
+    data = processor_goods_transection.data
+    # do something with the processed data
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from DataBaseConnection import DatabaseExporter as dbcon
 
 exporter = dbcon('root', '123qweasdzxcSq', 'localhost', 'easytrick')
 
-
-file_path_goods_transection = r"D:\monymovment\Cashflows\Excel_files\SBJRNLITMRPTTAX.xls"
-
-def process_good_transection(file_path):
+def process_good_transection():
+    file_path_goods_transection = r"D:\monymovment\Cashflows\Excel_files\SBJRNLITMRPTTAX.xls"
     expected_cols_goods_transection = ['Acc_Nm' ,'sPrc', 'sQty', 'spkid']
     wanted_cols_goods_transection = ['tr_dt', 'TR_NO', 'tr_ds', 'Text103', 'Acc_Nm']
     filter_column_goods_tran = 'tr_ds'
     filter_values = 'بيع|مرتجع'
-    processor_goods_transection =dprs(file_path, expected_cols_goods_transection)
+    processor_goods_transection =dprs(file_path_goods_transection, expected_cols_goods_transection)
     processor_goods_transection.read_data()
     processor_goods_transection.select_columns(wanted_cols_goods_transection)
     processor_goods_transection.filter_row(filter_column_goods_tran, filter_values)
@@ -24,13 +22,10 @@ def process_good_transection(file_path):
     processor_goods_transection.select_columns(wanted_cols_goods_transection)
     processor_goods_transection.groupby_agg(['InvoiceID'],{'Total_invoice':'sum','tr_dt':'first','Acc_Nm':'first'})
     return processor_goods_transection
-Processor_goods_transection = process_good_transection(file_path_goods_transection)
-print(Processor_goods_transection.data)
-
-file_path_clints_data= r"D:\monymovment\Cashflows\Excel_files\SBAccMFDtlRpt.xls"
-
-def processor_Clints(file_path):
-    processor_Clints = dprs(file_path, ['rec_id', 'acc_cd', 'acc_nm', 'Text85', 'نص93'])
+Processor_goods_transection = process_good_transection()
+def processor_Clints():
+    file_path_clints_data= r"D:\monymovment\Cashflows\Excel_files\SBAccMFDtlRpt.xls"
+    processor_Clints = dprs(file_path_clints_data, ['rec_id', 'acc_cd', 'acc_nm', 'Text85', 'نص93'])
     processor_Clints.read_data()
     processor_Clints.select_columns(['acc_nm', 'Text85', 'نص93'])
     processor_Clints.filter_row_null('acc_nm')
@@ -38,21 +33,7 @@ def processor_Clints(file_path):
     return processor_Clints
 
 
-processor_clints_data = processor_Clints(file_path_clints_data)
-
-print(processor_clints_data.data)
-
-exporter.export_data(Processor_goods_transection.data, 'goodstransectionte')
-exporter.delete_data(Processor_goods_transection.data, 'goodstransectionte', 'InvoiceID', 'removed_rows')
-exporter.export_data_frist(processor_clints_data.data, 'clints_data')
-
-exporter = dbcon('root', '123qweasdzxcSq', 'localhost', 'easytrick')
-file_path_goods_transection = r"D:\monymovment\Cashflows\Excel_files\SBJRNLITMRPTTAX.xls"
-def process_good_transection(file_path):......
-Processor_goods_transection = process_good_transection(file_path_goods_transection)
-file_path_clints_data= r"D:\monymovment\Cashflows\Excel_files\SBAccMFDtlRpt.xls"
-def processor_Clints(file_path):......
-processor_clints_data = processor_Clints(file_path_clints_data)
+processor_clints_data = processor_Clints()
 exporter.export_data(Processor_goods_transection.data, 'goodstransectionte')
 exporter.delete_data(Processor_goods_transection.data, 'goodstransectionte', 'InvoiceID', 'removed_rows')
 exporter.export_data_frist(processor_clints_data.data, 'clints_data')
