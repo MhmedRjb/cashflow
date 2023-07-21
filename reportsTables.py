9903e21cb7f72db35e9d6f27814921abe3605cc4
mohamed <mohamed2elfateh@gmail.com>
2023-07-21 23:48:58 +0300

    before sweeping what i left under carpet

--- reportsTables.py
+++ reportsTables.py
@@ -16,45 +16,20 @@ def display_data():
 @displaytables_bp.route('/Elfateh/main/reports/cashflow')
 @displaytables_bp.route('/Elfateh/main/reports/cashflow/display_goodstransectionte')
 def display_goodstransectionte():    
-    data = exporter.readsql(  f""" SELECT 
-CONCAT('(', GROUP_CONCAT(DISTINCT CONCAT('''', InvoiceID, '''')), ',', ')') AS InvoiceID ,
-SUM(CASE WHEN InvoiceID LIKE 'Ba%%' THEN Total_invoice ELSE -Total_invoice END) AS Total_invoice,
-tr_dt, 
-Acc_Nm, 
-MAX(dueDate) as dueDate, 
-max(realDate) as realDate,
-(CASE WHEN SUM(Paid) = COUNT(*) THEN 1 ELSE NULL END) AS Paid, 
-SUM(CASE WHEN InvoiceID LIKE 'Ba%%' THEN getpaid ELSE -getpaid END) AS getpaid,
-SUM(CASE WHEN InvoiceID LIKE 'Ba%%' THEN total_invoice_aftertax ELSE -total_invoice_aftertax END) AS total_invoice_aftertax,
-SUM(CASE WHEN InvoiceID LIKE 'Ba%%' THEN leftUnPaid ELSE -leftUnPaid END) AS leftUnPaid 
-FROM mainsales_py where realDate >'2023-07-07'
-OR realDate is NULL 
-GROUP BY Acc_Nm, tr_dt 
-ORDER by Paid desc ,dueDate
- """)
+    data = exporter.readsql(  f""" SELECT * from cashflowGroupAcc_Nm__tr_dt""")
     current_date = get_current_date()
     return render_template('audra.html', data=data, current_date=current_date)
 
 @displaytables_bp.route('/Elfateh/main/reports/cashflow/display_all_goodstransectionte')
 def display_all_goodstransectionte():
 
-    data = exporter.readsql(f"""
-        SELECT
-            CONCAT('(', '''', InvoiceID, '''', ',', ')') AS InvoiceID
-            , total_invoice as Total_invoice
-            , tr_dt
-            , Acc_Nm
-            , dueDate
-            , realDate
-            , (CASE WHEN Paid is NULL THEN 0 ELSE Paid END)  AS Paid
-            , getpaid
-            , total_invoice_aftertax
-            , leftUnPaid as leftUnPaid
-        FROM mainsales_py
-        where realDate = '{get_current_date()}'
-        OR realDate is NULL
-        ORDER by  Paid desc ,dueDate
-        """)
+    data = exporter.readsql(f"""SELECT *  from cashflowFilter__aftertoday""")
+    current_date = get_current_date()
+    return render_template('audra.html', data=data, current_date=current_date)
+
+@displaytables_bp.route('/Elfateh/main/reports/cashflow/display_cashflowgroup__comapnyname')
+def cashflowgroup__comapnyname():
+    data = exporter.readsql(f"""SELECT *  from cashflowgroup__comapnyname""")
     current_date = get_current_date()
     return render_template('audra.html', data=data, current_date=current_date)
 
@@ -62,6 +37,6 @@ def display_all_goodstransectionte():
 
 @displaytables_bp.route('/Elfateh/main/reports/cashflow/display_goodstransectionte_summary')
 def display_goodstransectionte_summary():
-    data = exporter.readsql("SELECT * FROM cashflowsummary WHERE leftUnPaid <>0")
+    data = exporter.readsql("SELECT * FROM cashflow__summary WHERE leftUnPaid <>0")
     current_date = get_current_date()
     return render_template('miller.html', data=data, current_date=current_date)
